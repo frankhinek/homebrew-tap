dcf9d31e84affd5025731bfa1bfe69f1699ba7ca
Frank Hinek <frank.hinek@gmail.com>
2018-02-25 18:48:37 -0500

    Add AutoMounter Helper cask

---
+++ Casks/automounter-helper.rb
@@ -0,0 +1,10 @@
+cask 'automounter-helper' do
+  version '1.3.1'
+  sha256 '1141ed678313a4061170abdef5e81a0a6d14ee73914b7c4ee2a8a83b047521d3'
+
+  url 'https://www.pixeleyes.co.nz/automounter/helper/AutoMounterHelper.dmg'
+  name 'AutoMounter Helper'
+  homepage 'https://www.pixeleyes.co.nz/automounter/helper/index.html'
+
+  installer manual: 'AutoMounterHelper.app'
+end
