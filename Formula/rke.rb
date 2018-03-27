e771fc6078e0733539aa00e8957b38535ffc3d7d
Frank Hinek <accounts@hinek.com>
2018-03-27 12:13:19 -0700

    Update RKE to v0.1.3

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -1,9 +1,9 @@
 class Rke < Formula
   desc "Rancher Kubernetes Engine, K8s installation"
   homepage "https://github.com/rancher/rke"
-  url "https://github.com/rancher/rke/releases/download/v0.1.3-rc1/rke_darwin-amd64"
-  version "0.1.3-rc1"
-  sha256 "9917f3f2836764b0d711b50902eb7d6a644f0178ced74efaf7501d880cf0620a"
+  url "https://github.com/rancher/rke/releases/download/v0.1.3/rke_darwin-amd64"
+  version "0.1.3"
+  sha256 "33b76fd413a1ed7114a268409fa75cc7d7934ad70b9b6cc2d18e869a1a093773"
   head "https://github.com/rancher/rke.git"
 
   def install
